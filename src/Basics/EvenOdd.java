422a86c108e3128197376b0b3d6d1933fd229bd6
Mario F <110302226+thelastmario@users.noreply.github.com>
2026-03-13 17:30:25 -0400

    Even or odd.

---
+++ src/Basics/EvenOdd.java
@@ -0,0 +1,13 @@
+package Basics;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 9;
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        }  else {
+            System.out.println("Odd");
+        }
+    }
+}
